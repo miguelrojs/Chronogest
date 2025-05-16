2225e445cea7ca5d4c59fa9a58ca27021710799e
miguelrojs <164452948+miguelrojs@users.noreply.github.com>
2025-05-16 00:54:52 -0500

    cambio

--- Assets/js/gestion-usuarios.js
+++ Assets/js/gestion-usuarios.js
@@ -1,29 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const listaUsuarios = document.getElementById('usuariosLista');  // Obtener el contenedor de la lista de usuarios
 
-    if (!listaUsuarios) {
-        console.error("No se encontró el contenedor #usuariosLista");
-        return;
-    }
 
-    // Obtener todos los usuarios desde localStorage
-    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+   document.addEventListener('DOMContentLoaded', function() {
+       const listaUsuarios = document.getElementById('usuariosLista');  // Obtener el contenedor de la lista de usuarios
 
-    // Comprobar si hay usuarios registrados
-    if (usuarios.length === 0) {
-        listaUsuarios.innerHTML = "<p>No hay usuarios registrados.</p>";
-        return;
-    }
+       if (!listaUsuarios) {
+           console.error("No se encontró el contenedor #usuariosLista");
+           return;
+       }
 
-    // Crear la lista de usuarios y mostrarla
-    const ul = document.createElement('ul');
-    
-    usuarios.forEach(usuario => {
-        const li = document.createElement('li');
-        li.textContent = `${usuario.nombre} (${usuario.rolUsuario}) - ${usuario.correo}`;  // Mostrar nombre, rol y correo
-        ul.appendChild(li);
-    });
+       // Obtener todos los usuarios desde localStorage
+       const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-    // Añadir la lista de usuarios al contenedor
-    listaUsuarios.appendChild(ul);
-});
+       // Comprobar si hay usuarios registrados
+       if (usuarios.length === 0) {
+           listaUsuarios.innerHTML = "<p>No hay usuarios registrados.</p>";
+           return;
+       }
+
+       // Crear la lista de usuarios y mostrarla
+       const ul = document.createElement('ul');
+       
+       // Itera sobre todos los usuarios en localStorage
+       usuarios.forEach(usuario => {
+           const li = document.createElement('li');
+           li.textContent = `${usuario.nombre} (${usuario.rolUsuario}) - ${usuario.correo}`;  // Mostrar nombre, rol y correo
+           ul.appendChild(li);
+       });
+
+       // Añadir la lista de usuarios al contenedor
+       listaUsuarios.appendChild(ul);
+   });
+
